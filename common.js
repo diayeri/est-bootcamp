be8f956d5ce6cfd6f29caaa90a12a96257a61933
dia <jdyoung1031@gmail.com>
2024-07-04 14:47:27 +0900

    currentPath 업데이트

--- common.js
+++ common.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   console.log(currentPath);
   // 메인 페이지와 하위 페이지에 따라 다른 동작 수행
-  if (currentPath.endsWith("index.html")) {
+  if (currentPath.endsWith("index.html" || "est-bootcamp/")) {
     // 메인 페이지: 리스트 항목 추가
     loadPageContent();
   } else {
