7048d1335794e5cc140f9e1381b955f045b70775
dia <jdyoung1031@gmail.com>
2024-08-14 11:36:08 +0900

    JSX 실습

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,7 +1,33 @@
+import "./App.css";
+
 function App() {
+  const time = new Date();
+  const timeStyle = { backgroundColor: "black", color: "white" };
+  const list = [
+    { no: 1, area: "대전", visited: false },
+    { no: 2, area: "부산", visited: true },
+    { no: 3, area: "목포", visited: false },
+    { no: 4, area: "제주도", visited: false },
+  ];
   return (
-    <div>
-      hello world
+    <div className="div">
+      <h1>time: {time.toString()}</h1>
+      <div className="flex">
+        <h2 style={{ ...timeStyle, color: "red" }}>{time.getFullYear()}년</h2>
+        <h2 style={timeStyle}>
+          {time.getMonth() + 1}월 {time.getDate()}일 {time.getDay()}요일
+        </h2>
+        <h2 style={timeStyle}>
+          {time.getHours()}시 {time.getMinutes()}분 {time.getSeconds()}초
+        </h2>
+      </div>
+      <ol>
+        {list.map((i) => (
+          <li className={i.visited ? "visited" : ""} key={i.no}>
+            {i.area}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
