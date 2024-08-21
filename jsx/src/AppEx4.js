356d563a185983eab83ae6923166ddfd1fa4019b
dia <jdyoung1031@gmail.com>
2024-08-21 10:03:03 +0900

    예제4 진행중 (펫 리스트)

--- jsx/src/AppEx4.js
+++ jsx/src/AppEx4.js
@@ -3,34 +3,22 @@ import PetFieldset from "./components/ex04/PetFieldset";
 import PetList from "./components/ex04/PetList";
 import "./App.css";
 
-const data = [
-  { name: "벨라", species: "고양이", age: "5", id: 111 },
-  { name: "루시", species: "강아지", age: "3", id: 112 },
-  { name: "데이지", species: "토끼", age: "2", id: 113 },
-  { name: "몰리", species: "고양이", age: "1", id: 114 },
-  { name: "매기", species: "강아지", age: "6", id: 115 },
-];
-
 function App() {
-  const [name, setName] = useState();
-  const [species, setSpecies] = useState();
-  const [age, setAge] = useState();
+  const [pets, setPets] = useState([
+    { name: "벨라", species: "고양이", age: "5", id: 111 },
+    { name: "루시", species: "강아지", age: "3", id: 112 },
+    { name: "데이지", species: "토끼", age: "2", id: 113 },
+    { name: "몰리", species: "고양이", age: "1", id: 114 },
+    { name: "매기", species: "강아지", age: "6", id: 115 },
+  ]);
 
-  const submitNew = () => {
-    const newData = new Object();
-    newData = {};
-  };
+  const addNew = () => {};
 
   return (
     <>
       <h1>애완동물 정보 리스트</h1>
-      <PetFieldset
-        setName={setName}
-        setSpecies={setSpecies}
-        setAge={setAge}
-        submit={submitNew}
-      />
-      <PetList data={data} />
+      <PetFieldset setPets={setPets} />
+      <PetList data={pets} />
     </>
   );
 }
