813fb8b84282b50df243a8bcbf772fa6be555092
dia <jdyoung1031@gmail.com>
2024-08-22 15:41:30 +0900

    합성 컴포넌트 실습

---
+++ jsx/src/AppEx7.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProductImage = styled.img`
+  width: 100%;
+  aspect-ratio: 1/1;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+
+const ProductTitle = styled.h2`
+  font-size: xx-large;
+  font-weight: 500;
+`;
+
+const ProductSubTitle = styled.h3`
+  font-size: x-large;
+  font-weight: 500;
+`;
+
+const ProductDescription = styled.p``;
+
+const CardWrapper = styled.div`
+  display: inline-block;
+  width: 400px;
+  padding: 1em;
+  border: 1px solid green;
+  border-radius: 20px;
+`;
+
+function ProductPrice({ unit }) {
+  return <p>100,000{unit}</p>;
+}
+
+function ContCard(props) {
+  return <CardWrapper>{props.children}</CardWrapper>;
+}
+
+export default function AppEx7() {
+  return (
+    <ContCard>
+      <ProductImage
+        src={"https://picsum.photos/id/237/200/300"}
+        alt={"검은강아지"}
+      />
+      <ProductTitle>검은 강아지를 입양하세요</ProductTitle>
+      <ProductSubTitle>엄청 얌전하답니다. 우리개는 안물어요!</ProductSubTitle>
+      <ProductDescription>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+        distinctio sed molestias saepe reiciendis fugit illum enim et inventore,
+        aliquam esse non nam consectetur minima atque consequuntur voluptates,
+        eum quia.
+      </ProductDescription>
+      <ProductPrice unit="원" />
+    </ContCard>
+  );
+}
