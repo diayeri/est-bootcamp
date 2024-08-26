52bcc0e6fe5b8a26902b13164bf3e721e815f31e
dia <jdyoung1031@gmail.com>
2024-08-26 09:33:57 +0900

    UseEffect 예제 업데이트

--- jsx/src/AppUseEffect.js
+++ jsx/src/AppUseEffect.js
@@ -1,23 +1,38 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  const handleCountUp = (e) => {
-    setCount(count + 1);
-  };
+function Time(props) {
+  const [today, setToday] = useState(new Date());
+  const hour = today.getHours();
+  const min = today.getMinutes();
+  const sec = today.getSeconds();
+
+  console.log("렌더링이 됩니다..?");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const t = new Date();
+      setToday(t);
+    }, 1000);
+    return () => {
+      // clearInterval : setInterval 함수의 작동을 중단합니다.
+      clearInterval(intervalId);
+      console.log(intervalId);
+    };
+  }, [today]);
 
   return (
-    <>
-      <div>{count}</div>
-      <button onClick={handleCountUp}>Up!</button>
-    </>
+    <div>
+      <h1>
+        시간 : {hour}시 {min}분 {sec}초
+      </h1>
+    </div>
   );
 }
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
+    <div>
+      <Time />
     </div>
   );
 }
