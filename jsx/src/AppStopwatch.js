f6daa02b67469e5174fd87a4b5ef29a4a0b0655a
dia <jdyoung1031@gmail.com>
2024-08-26 15:39:41 +0900

    스톱와치 정리 - start, stop 기능

--- jsx/src/AppStopwatch.js
+++ jsx/src/AppStopwatch.js
@@ -2,35 +2,27 @@ import { useRef, useState } from "react";
 
 export default function Stopwatch() {
   // 시작한 시간
-  const startTime = useRef(null);
-  // 현재 시간
-  const [now, setNow] = useState(null);
+  const startTimeRef = useRef(0);
   // 인터벌함수의 id
-  const intervalId = useRef(null);
+  const intervalIdRef = useRef(null);
+  // 경과 시간
+  const [secondsPassed, setSecondPassed] = useState(0);
 
   function handleStart() {
-    startTime.current = Date.now();
-    setNow(Date.now());
+    startTimeRef.current = Date.now();
 
-    intervalId.current = setInterval(() => {
-      setNow(Date.now());
+    intervalIdRef.current = setInterval(() => {
+      setSecondPassed(Date.now() - startTimeRef.current + secondsPassed);
     }, 10);
   }
 
   function handleStop() {
-    clearInterval(intervalId.current);
-    console.log(intervalId.current);
-    console.log(startTime.current, now);
-  }
-
-  let secondsPassed = 0;
-  if (startTime && now) {
-    secondsPassed = (now - startTime.current) / 1000; // 기본 단위가 밀리세컨드이기 때문에 초단위로 표현하기 위해서 1000을 나눕니다.
+    clearInterval(intervalIdRef.current);
   }
 
   return (
     <>
-      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <h1>Time passed: {(secondsPassed / 1000).toFixed(3)}</h1>
       <button onClick={handleStop}>Reset</button>
       <button onClick={handleStart}>Start</button>
       <button onClick={handleStop}>Stop</button>
