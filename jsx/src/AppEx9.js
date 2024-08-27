4345f44c04a16f7ab41974a397fed7be261e8bea
dia <jdyoung1031@gmail.com>
2024-08-27 15:13:53 +0900

    addCart 기능 추가

--- jsx/src/AppEx9.js
+++ jsx/src/AppEx9.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 // import { productsInfo } from "./context/ProductsInfo";
 
 const CartContext = createContext();
@@ -6,7 +6,22 @@ const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   // 카트에 상품 추가하기
-  const addCart = () => {};
+  const addCart = (product) => {
+    setCart((prevCart) => {
+      // 카트에 이미 상품이 존재하는지
+      const isExist = prevCart.find((item) => product.id === item.id);
+
+      // 상품이 이미 존재하는 경우
+      if (isExist) {
+        return prevCart.map((item) =>
+          item.id === product.id ? { ...item, count: item.count + 1 } : item
+        );
+      }
+
+      // 상품이 처음 들어가는 경우
+      return [...prevCart, { ...product, count: 1 }];
+    });
+  };
 
   // 카트에서 제거하기
   const removeCart = () => {};
@@ -34,6 +49,8 @@ function Header() {
 }
 
 function ProductList() {
+  const { addToCart } = useContext(CartContext);
+
   const products = [
     { id: 1, name: "노트북", price: 1000 },
     { id: 2, name: "스마트폰", price: 500 },
@@ -47,7 +64,9 @@ function ProductList() {
         {products.map((product) => (
           <li key={product.id}>
             {product.name} - ₩{product.price}
-            <button type="button">카트에 추가</button>
+            <button type="button" onClick={() => addToCart(product)}>
+              카트에 추가
+            </button>
           </li>
         ))}
       </ul>
