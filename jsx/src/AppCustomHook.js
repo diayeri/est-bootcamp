bcacf8dc219b9e9fbf06e5edb8ebd3185eaee5a3
dia <jdyoung1031@gmail.com>
2024-08-29 14:29:40 +0900

    이미지 불러오기 오류 수정

--- jsx/src/AppCustomHook.js
+++ jsx/src/AppCustomHook.js
@@ -22,7 +22,8 @@ const AppCustomHook = () => {
       );
       if (!req.ok) throw new Error();
       const json = await req.json();
-      setImageData(json);
+
+      setImageData((prevImages) => [...prevImages, ...json]);
       setIsLoading(false);
     } catch (err) {
       console.error("ERR", err);
@@ -30,24 +31,17 @@ const AppCustomHook = () => {
     }
   };
 
-  useEffect(() => {
-    fetchImages();
-  }, []);
-
   // 바닥에 닿으면 새로 fetch 데이터 가져와서
   // 추가로 가져온 이미지 보여주기
   useEffect(() => {
     if (isBottom) {
       // 다음사진을 다운받으시오
       setPageNum((prevPage) => prevPage + 1);
-      console.log(pageNum);
-      // fetchImages(page);
     }
   }, [isBottom]);
 
   // useEffect는 fetch실행시만 적용해도 됨
   useEffect(() => {
-    // 추가된것만 새로 불러오는 방법이 있을텐데...
     console.log(pageNum);
     fetchImages(pageNum);
   }, [pageNum]);
