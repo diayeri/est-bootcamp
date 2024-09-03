2c314174a660ef2e101121790f97ac6c2aab6254
dia <jdyoung1031@gmail.com>
2024-09-03 16:08:41 +0900

    jest 예제 오류 수정

--- TDD/jest/src/App.js
+++ TDD/jest/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newColor = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button
+          style={{ backgroundColor: buttonColor }}
+          onClick={() => setButtonColor(newColor)}
+        >
+          change to {newColor}!
+        </button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- TDD/jest/src/App.test.js
+++ TDD/jest/src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("버튼 작동 테스트", () => {
+  render(<App />);
+  const button = screen.getByRole("button", { name: "change to blue!" });
+  expect(button).toHaveStyle({ backgroundColor: "red" });
+
+  fireEvent.click(button);
+  expect(button).toHaveStyle({ backgroundColor: "blue" });
+  expect(button.textContent).toBe("change to red!");
 });
