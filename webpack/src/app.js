ae1fb2b83bb2a4a078db2f9b522d9f940c908dad
dia <jdyoung1031@gmail.com>
2024-09-05 14:18:45 +0900

    base64 이미지 로더조건

--- webpack/src/app.js
+++ webpack/src/app.js
@@ -1,4 +1,5 @@
 import plus from "./plus.js";
+import image64 from "./android.png";
 import "./styles.css";
 
 console.log(plus(10, 4));
@@ -6,3 +7,7 @@ console.log(plus(10, 4));
 function minus(a, b) {
   return a - b;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = `<img src="${image64}"/>`;
+});
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -16,19 +16,32 @@ module.exports = {
   module: {
     rules: [
       // 여기서 로더를 추가할 수 있습니다.
-      {
-        // 로더가 처리해야할 파일의 패턴(정규표현식)입니다.
-        // 여기서 \는 .을 정규표현식 문법이 아닌 특수문자로, .js$ 는 .js 로 끝나는 모든 파일을 의미합니다.
-        // 원래 정규표현식에서 .의 의미는 모든 문자나 숫자를 의미합니다.
-        test: /\.js$/,
-        use: [
-          // 위와 일치하는 패턴의 파일이 전달될 로더를 설정합니다.
-          path.resolve("./myLoader.js"),
-        ],
-      },
+      // {
+      //   // 로더가 처리해야할 파일의 패턴(정규표현식)입니다.
+      //   // 여기서 \는 .을 정규표현식 문법이 아닌 특수문자로, .js$ 는 .js 로 끝나는 모든 파일을 의미합니다.
+      //   // 원래 정규표현식에서 .의 의미는 모든 문자나 숫자를 의미합니다.
+      //   test: /\.js$/,
+      //   use: [
+      //     // 위와 일치하는 패턴의 파일이 전달될 로더를 설정합니다.
+      //     path.resolve("./myLoader.js"),
+      //   ],
+      // },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      // {
+      //   test: /\.(png|jpg|gif|svg)$/,
+      //   type: "asset/inline",
+      // },
+      {
+        test: /\.(png|jpg|gif|svg|jpeg|webp)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 20 * 1024, // 1kb가 1024byte 이기 때문에 20kb를 원한다면 1024에 20을 곱합니다.
+          },
+        },
       },
     ],
   },
