e4af1d639d6d5b75d9bf9433101ce27578210642
dia <jdyoung1031@gmail.com>
2024-09-12 15:09:59 +0900

    로딩상태관리 추가

--- typescript/todo-react-ts/src/hooks/todoHook.ts
+++ typescript/todo-react-ts/src/hooks/todoHook.ts
@@ -11,14 +11,13 @@ function useTodoData(): [TodoItem[], (todoText: string) => void, boolean] {
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch("http://localhost:3300/todos")
       .then((res) => {
         return res.json();
       })
       .then((todoData) => {
-        // 이 상태에서는 반복해서 감지,작동함 -> useEffect 추가하기
-        // - 컴포넌트 최상단: 컴포넌트가 렌더링 될때 실행됨 (=함수실행)
-        // - 비교하기 위해 실행을 하다가 또 실행을 하게 되는?거 (무한반복)
+        setIsLoading(false);
         setTodoList(todoData);
       });
   }, []);
@@ -26,6 +25,7 @@ function useTodoData(): [TodoItem[], (todoText: string) => void, boolean] {
   // handleBtn 처럼 컴포넌트(클라이언트), 서버 둘다 의존하는 경우는?
   // - 내부 코드를 나누어서 작업할것
   const postTodo = (todoText: string): void => {
+    setIsLoading(true);
     fetch("http://localhost:3300/todos", {
       method: "POST",
       headers: {
@@ -37,9 +37,8 @@ function useTodoData(): [TodoItem[], (todoText: string) => void, boolean] {
         return res.json();
       })
       .then((newTodo) => {
+        setIsLoading(false);
         setTodoList((prevTodoList) => {
-          // prevTodoList.push() 를 하면 prevTodoList가 바뀐게 아니다 (내부 요소만 변함, 변화감지 못함)
-          // 리액트에서는 props/state가 바뀌면 리렌더링을 한다
           return [...prevTodoList, newTodo];
         });
       });
