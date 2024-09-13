6e9553d50c46a67018d26b698553a22b09907dbf
dia <jdyoung1031@gmail.com>
2024-09-13 15:34:40 +0900

    updateProfile, 오류처리

--- firebase/fireauth/src/hooks/useSignup.js
+++ firebase/fireauth/src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { appAuth } from "../firebase/config";
 
 export const useSignup = () => {
@@ -21,6 +21,20 @@ export const useSignup = () => {
         // 회원가입을 통해 로그인한 유저의 정보
         const user = userCredential.user;
         console.log(user);
+        if (!user) {
+          throw new Error("회원가입에 성공했습니다.");
+        }
+
+        updateProfile(appAuth.currentUser, { displayName })
+          .then(() => {
+            // Profile updated! ...
+            setIsPending(false);
+          })
+          .catch((error) => {
+            setError(error.message);
+            console.error(error);
+            setIsPending(false);
+          });
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -28,6 +42,7 @@ export const useSignup = () => {
         // ..
         setError(error.message);
         console.error(`err code: ${errorCode}, ${error}`);
+        setIsPending(false);
       });
   };
 };
