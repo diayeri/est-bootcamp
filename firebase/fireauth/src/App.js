07b27a50a439d849c7b1d7c4c6d7217e7625105b
dia <jdyoung1031@gmail.com>
2024-09-19 15:34:37 +0900

    로그인상태에 따라 페이지 리다이렉트 처리 - Navigate 태그 이용

--- firebase/fireauth/src/App.js
+++ firebase/fireauth/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
@@ -6,7 +6,7 @@ import Header from "./components/header/Header";
 import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
-  const { isAuthReady } = useAuthContext();
+  const { isAuthReady, user } = useAuthContext();
 
   return (
     <div className="App">
@@ -14,9 +14,20 @@ function App() {
         <BrowserRouter>
           <Header />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
+            <Route
+              path="/"
+              element={
+                user ? <Home /> : <Navigate replace={true} to="/login" />
+              }
+            ></Route>
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate replace={true} to="/" />}
+            ></Route>
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate replace={true} to="/" />}
+            ></Route>
           </Routes>
         </BrowserRouter>
       ) : (
