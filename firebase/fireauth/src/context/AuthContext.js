b382550dfde658cb840b58a26a073e8f3e7f60ca
dia <jdyoung1031@gmail.com>
2024-09-19 11:19:00 +0900

    reducer 상태관리 추가

--- firebase/fireauth/src/context/AuthContext.js
+++ firebase/fireauth/src/context/AuthContext.js
@@ -1,11 +1,26 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 // context
 const AuthContext = createContext();
 
+// dispatch를 통해 호출할 reducer 함수
+const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
 // component
 const AuthContextProvider = ({ children }) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [state, dispatch] = useReducer(authReducer, { user: null });
+
+  return (
+    // 보통 상태값은 전개구문으로 복사하여 전달
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthContextProvider };
